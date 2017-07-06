166e36fa762e0c6a0ce26c3c1544cd895e947996
Radoslav Georgiev <rgeorgiev583@gmail.com>
2017-07-07 02:51:18 +0300

    added mix project files

---
+++ pmdb/lib/pmdb.ex
@@ -0,0 +1,18 @@
+defmodule Pmdb do
+  @moduledoc """
+  Documentation for Pmdb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pmdb.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
