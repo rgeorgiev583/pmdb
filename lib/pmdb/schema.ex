7c154dd3f4c3d193d6321a8f0548baa414978b0c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-07-02 21:46:14 +0300

    add schema

---
+++ lib/pmdb/schema.ex
@@ -0,0 +1,12 @@
+defmodule Pmdb.Schema do
+  @type primitive_data() :: boolean() | number() | binary() | bitstring()
+  @type primitive_data_type() :: :boolean | :integer | :float | :binary | :bitstring | :string
+  @type data() ::
+          primitive_data()
+          | [data()]
+          | %{required(String.t()) => data()}
+  @type data_type() ::
+          primitive_data_type()
+          | [data_type()]
+          | %{required(String.t()) => data_type()}
+end
