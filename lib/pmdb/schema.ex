6f3de3d1720d901029755887220dfb207986fd56
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-07-03 01:05:03 +0300

    add annotated data type to schema

--- lib/pmdb/schema.ex
+++ lib/pmdb/schema.ex
@@ -1,6 +1,13 @@
 defmodule Pmdb.Schema do
   @type primitive_data() :: boolean() | number() | binary() | bitstring()
   @type primitive_data_type() :: :boolean | :integer | :float | :binary | :bitstring | :string
+  @type annotated_primitive_data() ::
+          {:boolean, boolean()}
+          | {:integer, integer()}
+          | {:float, float()}
+          | {:binary, binary()}
+          | {:bitstring, bitstring()}
+          | {:string, String.t()}
   @type data() ::
           primitive_data()
           | [data()]
@@ -9,4 +16,8 @@ defmodule Pmdb.Schema do
           primitive_data_type()
           | [data_type()]
           | %{required(String.t()) => data_type()}
+  @type annotated_data() ::
+          annotated_primitive_data()
+          | {:list, [annotated_data()]}
+          | {:map, %{required(String.t()) => annotated_data()}}
 end
