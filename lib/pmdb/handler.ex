8ca086088f7da14a178f2b5ed53ca7df1f0aa43c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-07-06 18:09:07 +0300

    convert the Pmdb.Handler behavior to a protocol

--- lib/pmdb/handler.ex
+++ lib/pmdb/handler.ex
@@ -1,11 +1,18 @@
-defmodule Pmdb.Handler do
-  @callback get(path :: String.t()) :: {:ok, Schema.data()} | {:error, String.t()}
-  @callback post(path :: String.t(), value :: Schema.data()) :: :ok | {:error, String.t()}
-  @callback put(path :: String.t(), value :: Schema.data()) :: :ok | {:error, String.t()}
-  @callback delete(path :: String.t()) :: :ok | {:error, String.t()}
-  @callback patch(path :: String.t(), delta :: Schema.data_delta()) :: :ok | {:error, String.t()}
+defprotocol Pmdb.Handler do
+  @spec get(path :: String.t()) :: {:ok, Schema.data()} | {:error, String.t()}
+  def get(path)
+  @spec post(path :: String.t(), value :: Schema.data()) :: :ok | {:error, String.t()}
+  def post(path, value)
+  @spec put(path :: String.t(), value :: Schema.data()) :: :ok | {:error, String.t()}
+  def put(path, value)
+  @spec delete(path :: String.t()) :: :ok | {:error, String.t()}
+  def delete(path)
+  @spec patch(path :: String.t(), delta :: Schema.data_delta()) :: :ok | {:error, String.t()}
+  def patch(path, delta)
 
-  @callback describe(path :: String.t()) :: {:ok, Schema.data_type()} | {:error, String.t()}
-  @callback validate(path :: String.t(), schema :: Schema.data_type()) ::
+  @spec describe(path :: String.t()) :: {:ok, Schema.data_type()} | {:error, String.t()}
+  def describe(path)
+  @spec validate(path :: String.t(), schema :: Schema.data_type()) ::
               :ok | {:error, String.t()}
+  def validate(path, schema)
 end
