8a123aee85b09533bc9a5057d89e75f1ec43bab3
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-07-06 00:27:44 +0300

    fs-handler: move filesystem path conversions to file operation invocation sites

--- lib/pmdb/handlers/fs-handler.ex
+++ lib/pmdb/handlers/fs-handler.ex
@@ -15,60 +15,60 @@ defimpl Pmdb.Handler, for: Pmdb.FileHandler do
     component
   end
 
-  defp parse_path_and_do(handler, path_str, action) do
-    Pmdb.Path.parse_path_and_do(path_str, fn path ->
-      fs_path = path |> Enum.map(&get_component_str/1) |> Path.join()
-      abs_fs_path = Path.join(handler.root_path, fs_path)
-      action.(abs_fs_path)
-    end)
+  defp convert_path_to_fs_path(handler, path) do
+    fs_path = path |> Enum.map(&get_component_str/1) |> Path.join()
+    Path.join(handler.root_path, fs_path)
   end
 
-  defp get_impl(path, {:type, :directory}, {:ok, entries}) do
-    entries |> Map.new(fn key -> {key, get_impl(Path.join(path, key))} end)
+  defp get_impl(handler, path, {:type, :directory}, {:ok, entries}) do
+    entries |> Map.new(fn key -> {key, get_impl(handler, path ++ [key])} end)
   end
 
-  defp get_impl(_, {:type, :regular}, {:ok, value}) do
+  defp get_impl(_, _, {:type, :regular}, {:ok, value}) do
     :erlang.binary_to_term(value)
   end
 
-  defp get_impl(_, {:type, :directory}, error) do
+  defp get_impl(_, _, {:type, :directory}, error) do
     error
   end
 
-  defp get_impl(_, {:type, :regular}, error) do
+  defp get_impl(_, _, {:type, :regular}, error) do
     error
   end
 
-  defp get_impl(path, {:type, :directory}) do
-    result = File.ls(path)
-    get_impl(path, {:type, :directory}, result)
+  defp get_impl(handler, path, {:type, :directory}) do
+    fs_path = convert_path_to_fs_path(handler, path)
+    result = File.ls(fs_path)
+    get_impl(handler, path, {:type, :directory}, result)
   end
 
-  defp get_impl(path, {:type, :regular}) do
-    result = File.read(path)
-    get_impl(path, {:type, :regular}, result)
+  defp get_impl(handler, path, {:type, :regular}) do
+    fs_path = convert_path_to_fs_path(handler, path)
+    result = File.read(fs_path)
+    get_impl(handler, path, {:type, :regular}, result)
   end
 
-  defp get_impl(_, {:type, _}) do
+  defp get_impl(_, _, {:type, _}) do
     {:error, "unsupported file type"}
   end
 
-  defp get_impl(path, {:ok, info}) do
-    get_impl(path, {:type, info.type})
+  defp get_impl(handler, path, {:ok, info}) do
+    get_impl(handler, path, {:type, info.type})
   end
 
-  defp get_impl(_, error) do
+  defp get_impl(_, _, error) do
     error
   end
 
-  defp get_impl(path) do
-    file_stat = File.stat(path)
-    get_impl(path, file_stat)
+  defp get_impl(handler, path) do
+    fs_path = convert_path_to_fs_path(handler, path)
+    file_stat = File.stat(fs_path)
+    get_impl(handler, path, file_stat)
   end
 
   def get(handler, path_str) do
-    parse_path_and_do(handler, path_str, fn path ->
-      get_impl(path)
+    Pmdb.Path.parse_path_and_do(path_str, fn path ->
+      get_impl(handler, path)
     end)
   end
 
@@ -76,87 +76,92 @@ defimpl Pmdb.Handler, for: Pmdb.FileHandler do
     {:error, "not implemented"}
   end
 
-  defp put_impl(path, list, :write) when is_list(list) do
+  defp put_impl(handler, path, list, :write) when is_list(list) do
     list
     |> Enum.with_index()
     |> Enum.map(fn {value, index} ->
-      put_impl(Path.join(path, Integer.to_string(index)), value, :put)
+      put_impl(handler, path ++ [index], value, :put)
     end)
     |> Pmdb.Utility.reduce_results()
   end
 
-  defp put_impl(path, map, :write) when is_map(map) do
+  defp put_impl(handler, path, map, :write) when is_map(map) do
     map
-    |> Enum.map(fn {key, value} -> put_impl(Path.join(path, key), value, :put) end)
+    |> Enum.map(fn {key, value} -> put_impl(handler, path ++ [key], value, :put) end)
     |> Pmdb.Utility.reduce_results()
   end
 
-  defp put_impl(path, value, :write) do
+  defp put_impl(handler, path, value, :write) do
+    fs_path = convert_path_to_fs_path(handler, path)
     data = :erlang.term_to_binary(value)
-    File.write(path, data)
+    File.write(fs_path, data)
   end
 
-  defp put_impl(path, value, :put) do
-    dir_path = Path.dirname(path)
-    File.mkdir_p(dir_path)
-    put_impl(path, value, :write)
+  defp put_impl(handler, path, value, :put) do
+    {_, dir_path} = path |> List.pop_at(-1)
+    dir_fs_path = convert_path_to_fs_path(handler, dir_path)
+    File.mkdir_p(dir_fs_path)
+    put_impl(handler, path, value, :write)
   end
 
-  defp put_impl(path, value, {:ok, _}) do
-    File.rm_rf(path)
-    put_impl(path, value, :put)
+  defp put_impl(handler, path, value, {:ok, _}) do
+    fs_path = convert_path_to_fs_path(handler, path)
+    File.rm_rf(fs_path)
+    put_impl(handler, path, value, :put)
   end
 
-  defp put_impl(path, value, _) do
-    put_impl(path, value, :put)
+  defp put_impl(handler, path, value, _) do
+    put_impl(handler, path, value, :put)
   end
 
-  defp put_impl(path, value) do
-    file_stat = File.stat(path)
-    put_impl(path, value, file_stat)
+  defp put_impl(handler, path, value) do
+    fs_path = convert_path_to_fs_path(handler, path)
+    file_stat = File.stat(fs_path)
+    put_impl(handler, path, value, file_stat)
   end
 
   def put(handler, path_str, value) do
-    parse_path_and_do(handler, path_str, fn path ->
-      put_impl(path, value)
+    Pmdb.Path.parse_path_and_do(path_str, fn path ->
+      put_impl(handler, path, value)
     end)
   end
 
-  defp delete_impl(path) do
-    File.rm_rf(path)
+  defp delete_impl(handler, path) do
+    fs_path = convert_path_to_fs_path(handler, path)
+    File.rm_rf(fs_path)
   end
 
   def delete(handler, path_str) do
-    parse_path_and_do(handler, path_str, fn path ->
-      delete_impl(path)
+    Pmdb.Path.parse_path_and_do(path_str, fn path ->
+      delete_impl(handler, path)
     end)
   end
 
-  defp patch_impl(_, nil) do
+  defp patch_impl(_, _, nil) do
     :ok
   end
 
-  defp patch_impl(path, :drop) do
-    delete_impl(path)
+  defp patch_impl(handler, path, :drop) do
+    delete_impl(handler, path)
   end
 
-  defp patch_impl(path, {:data, data}) do
-    put_impl(path, data)
+  defp patch_impl(handler, path, {:data, data}) do
+    put_impl(handler, path, data)
   end
 
-  defp patch_impl(_, {:list, _}) do
+  defp patch_impl(_, _, {:list, _}) do
     {:error, "not implemented"}
   end
 
-  defp patch_impl(path, {:map, delta_map}) do
+  defp patch_impl(handler, path, {:map, delta_map}) do
     delta_map
-    |> Enum.map(fn {key, entry_delta} -> patch_impl(Path.join(path, key), entry_delta) end)
+    |> Enum.map(fn {key, entry_delta} -> patch_impl(handler, path ++ [key], entry_delta) end)
     |> Pmdb.Utility.reduce_results()
   end
 
   def patch(handler, path_str, delta) do
-    parse_path_and_do(handler, path_str, fn path ->
-      patch_impl(path, delta)
+    Pmdb.Path.parse_path_and_do(path_str, fn path ->
+      patch_impl(handler, path, delta)
     end)
   end
 end
